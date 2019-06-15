32791b9e4e8cbac4e3cca6baf4a51b1bc3df2d87
gapehorn <jordandmjones7@gmail.com>
2019-06-15 18:42:22 -0400

    added a comment

--- AcessControl/src/main.java
+++ AcessControl/src/main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class main {
 
 	public static void main(String[] args) throws IOException {
-			
+		//create the ACM object
 		accessControl acm = new accessControl();
 		acm.createUser("Jordan", "Administrator","Jordan");
 		String userInput = "";
